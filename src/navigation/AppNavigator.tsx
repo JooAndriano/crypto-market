68cf1cee6e84ef7c8ba624a19c98371de1e0269e
joshua <joshua@digitalsekuriti.id>
2026-05-30 12:44:27 +0700

    Initial Commit

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,19 @@
+import { createNativeStackNavigator }
+from "@react-navigation/native-stack";
+
+import LoginScreen
+from "../screens/LoginScreen";
+
+const Stack =
+  createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+      />
+    </Stack.Navigator>
+  );
+}
